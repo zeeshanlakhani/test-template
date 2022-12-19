06d39cfd0104a97dff625eab6a94f0aa800da21a
github-action-benchmark <github@users.noreply.github.com>
2022-12-19 08:08:04 +0000

    add Rust Benchmark (cargo) benchmark result for b06c645f26c37d6a4631573949c0d5f58127c989

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1671437284769,
+  "repoUrl": "https://github.com/zeeshanlakhani/test-template",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "zeeshan.lakhani@gmail.com",
+            "name": "Zeeshan Lakhani",
+            "username": "zeeshanlakhani"
+          },
+          "committer": {
+            "email": "zeeshan.lakhani@gmail.com",
+            "name": "Zeeshan Lakhani",
+            "username": "zeeshanlakhani"
+          },
+          "distinct": true,
+          "id": "b06c645f26c37d6a4631573949c0d5f58127c989",
+          "message": "init",
+          "timestamp": "2022-12-19T03:04:32-05:00",
+          "tree_id": "3c6c6e97241aa2a476714dd2ed4debb74687737e",
+          "url": "https://github.com/zeeshanlakhani/test-template/commit/b06c645f26c37d6a4631573949c0d5f58127c989"
+        },
+        "date": 1671437284425,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "add",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
